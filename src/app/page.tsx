b94b75fbce93b049b5d374f584d793207e56a23f
Akshay Jadhav <akshayjadhav2133@gmail.com>
2024-07-16 10:06:54 +0530

    complete the game logic and UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,227 @@
 import Link from "next/link";
+"use client"
+import { useState, useEffect } from 'react';
 
 export default function HomePage() {
+  const rows=30;
+  const cols=30;
+  let g=intialGrid();
+
+  const [grid, setGrid] = useState<number[][]>(g);
+  const [gameStart, setGameStart]=useState<boolean>(false);
+
+  function intialGrid(){
+    
+
+    let grid:number[][]= new Array(rows);
+
+    for (let i = 0; i < rows; i++) {
+      grid[i] = new Array(cols).fill(0);
+    }
+
+    function horizontal(r:number,colStart:number,colEnd:number){
+
+      for(let i=colStart;i<=colEnd;i++){
+        grid[r][i]=1
+      }
+    }
+    function vertical(c:number,rowStart:number,rowEnd:number){
+      for(let j=rowStart;j<=rowEnd;j++){
+        grid[j][c]=1
+      }
+    }
+    //--a---
+    horizontal(5,2,9);
+    vertical(2,6,13);
+    vertical(9,6,13);
+    horizontal(9,4,7);
+
+
+
+    ///--p-----
+    horizontal(5,11,18);
+    vertical(11,6,13);
+    vertical(18,6,9);
+    horizontal(9,13,18);
+
+
+
+    //--J---
+    horizontal(5,20,27);
+    vertical(27,6,13);
+    horizontal(13,20,27);
+    vertical(20,11,13);
+
+    ////C----
+    horizontal(19,4,12);
+    vertical(4,19,27);
+    horizontal(27,4,12);
+    
+
+
+    ///--R-----
+    horizontal(19,18,26);
+    vertical(18,19,27);
+    vertical(26,19,23);
+    horizontal(23,20,26);
+    grid[24][20]=1;
+    grid[25][21]=1;
+    grid[26][22]=1;
+    grid[27][23]=1;
+
+
+
+
+    return grid;
+  }
+  function createrandomGrid(){
+    
+
+    let grid:number[][]= new Array(rows);
+
+    for (let i = 0; i < rows; i++) {
+      grid[i] = new Array(cols).fill(0);
+    }
+
+    for(let i=0;i<rows;i++){
+      for(let j=0;j<cols;j++){
+        if(Math.random()>0.8){
+          grid[i][j]=1
+        } else{
+          grid[i][j]=0;
+        }  
+      }
+    }
+  
+    
+
+
+    return grid;
+  }
+
+  function calculateNeighbors(grid:number[][],x:number,y:number):number{
+
+    let totalNeighbors:number=0;
+
+    for(let i=-1;i<=1;i++){
+
+      for(let j=-1;j<=1;j++){
+
+        let r=x+i;
+        let c=y+j;
+
+        if(r >= 0 && c>=0 && r< rows && c < cols && !(i==0 && j==0)){
+          totalNeighbors=totalNeighbors+ grid[r][c];
+        }
+      }
+    }
+    return totalNeighbors;
+
+  }
+
+  function newGridArray(grid:number[][]):number[][]{
+    //all rules of game
+    let newgrid:number[][]= new Array(rows);
+
+    for (let i = 0; i < rows; i++) {
+      newgrid[i] = new Array(cols).fill(0);
+    }
+
+    //
+    for(let i=0;i<rows;i++){
+
+      for(let j=0;j<cols;j++){
+        const neighbors = calculateNeighbors(grid,i,j);
+        if(grid[i][j]==1 &&( neighbors<2 || neighbors>3)){
+          newgrid[i][j] = 0; 
+        }else if(grid[i][j] == 0 && neighbors == 3){
+          newgrid[i][j] = 1; 
+        }else{
+          newgrid[i][j] =grid[i][j];
+        }
+
+
+      }
+    }
+
+    // console.log(newgrid);
+    return newgrid;
+
+  }
+
+  // console.log(newGridArray(grid));
+  function handleStart(){
+    setGameStart(!gameStart);
+    
+  }
+
+  function handleRandom(){
+    let temp=createrandomGrid();
+    setGrid(temp);
+    setGameStart(false);
+  }
+
+  useEffect(()=>{
+    let timeoutId;
+
+    if(gameStart){
+        timeoutId = setTimeout(() => {
+      let g=newGridArray(grid);
+       setGrid(g);
+    }, 500);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+
+    
+    
+
+  },[grid,gameStart])
+
+
+  
+ 
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     <h1>Game Of Life</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center  text-white bg-[#0F172A]">
+      <h1 className="text-3xl font-bold">Game of life</h1>
+
+      
+
+<div className=" border-2 border-gray-500" style={{display:"grid", gridTemplateColumns:`repeat(${rows},15px)`}}>
+        {grid.map((row,i)=> row.map((c,k)=> <div key={k} 
+        className={` w-[15px] h-[15px] ${c==1 ? "bg-yellow-400": "bg-[0F172A]"}`} ></div>)
+        
+      )
+        }
+      </div>
+
+
+
+
+
+      <div className="flex gap-2 mt-4">
+
+
+        <button type="button" className={`${!gameStart ? 'rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600':'rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'} `} onClick={handleStart}>{`${!gameStart ? 'Start':'Stop'}`}</button>
+
+
+      
+
+  <button
+    type="button"
+    className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black" onClick={handleRandom}
+  >
+    Random
+  </button>
+  
+      </div>
+
+     
+     
+     
     </main>
   );
 }
